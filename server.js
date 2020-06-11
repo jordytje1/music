8cda9f262208d8b31eb3d04d402851dad5c90f20
Glitch (hello-express) <none>
2020-06-11 12:44:57 +0000

    🌪🍡 Checkpoint ./server.js:17206642/99

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ setInterval(() => {
  
 const setupCMD = "!setreactionrole"
 let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
-const roles = ["lol"];["banaan"];
-const reactions = ["✅"];["😂"];
+const roles = ["『👦』『member』"];
+const reactions = ["✅"];
 
 
 //Load up the bot...
